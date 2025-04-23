39592b619bc18537dded9d77a4c02f2108ffe1c2
student7898 <n01740597@humber.ca>
2025-04-22 21:18:41 -0400

    Added script.js , completed Part 1

---
+++ script.js
@@ -0,0 +1,17 @@
+// Part 1
+
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return "Product: " + this.name + ", Price: " + this.price + ", Quantity: " + this.quantity;
+    }
+}
